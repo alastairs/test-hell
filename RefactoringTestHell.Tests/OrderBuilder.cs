8e955256881957254fefab6f860d8ed51ca4340e
Alastair Smith <alastair.smith@red-gate.com>
2014-10-14 15:14:44 +0100

    Add a builder for Orders.

---
+++ RefactoringTestHell.Tests/OrderBuilder.cs
@@ -0,0 +1,32 @@
+﻿
+namespace RefactoringTestHell.Tests
+{
+    class OrderBuilder
+    {
+        private int id;
+        private Customer customer;
+
+        public OrderBuilder()
+        {
+            id = 1;
+            customer = new CustomerBuilder().Build();
+        }
+
+        public OrderBuilder WithId(int newId)
+        {
+            id = newId;
+            return this;
+        }
+
+        public OrderBuilder PlacedBy(Customer newCustomer)
+        {
+            customer = newCustomer;
+            return this;
+        }
+
+        public Order Build()
+        {
+            return new Order(id, customer);
+        }
+    }
+}
