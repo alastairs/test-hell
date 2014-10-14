e5cc1c03fa540d8f380ea760d206e3c0ca9c6e60
Alastair Smith <alastair.smith@red-gate.com>
2014-10-14 14:20:48 +0100

    Add Customer class.

---
+++ RefactoringTestHell/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace RefactoringTestHell
+{
+    public class Customer
+    {
+        public Customer(int id, string givenName, string familyName, string addressLine1, string addressLine2,
+                        string addressLine3, string postCode)
+        {
+            Id = id;
+            GivenName = givenName;
+            FamilyName = familyName;
+            AddressLine1 = addressLine1;
+            AddressLine2 = addressLine2;
+            AddressLine3 = addressLine3;
+            PostCode = postCode;
+        }
+
+        public int Id { get; private set; }
+        public string GivenName { get; private set; }
+        public string FamilyName { get; private set; }
+        public string AddressLine1 { get; private set; }
+        public string AddressLine2 { get; private set; }
+        public string AddressLine3 { get; private set; }
+        public string PostCode { get; private set; }
+    }
+}
