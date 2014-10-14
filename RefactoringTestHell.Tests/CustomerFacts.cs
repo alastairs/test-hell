0af378e0440f5f793f0f6ac4af09a61520b393c6
Alastair Smith <alastair.smith@red-gate.com>
2014-10-14 14:23:51 +0100

    Add test using Customer.

---
+++ RefactoringTestHell.Tests/CustomerFacts.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace RefactoringTestHell.Tests
+{
+    internal class CustomerFacts
+    {
+        [Fact]
+        public void Placing_An_Order_Adds_The_Order_To_The_Customers_Account()
+        {
+            // Arrange    
+            var customer = new Customer(
+                1, // Id        
+                "Joe", "Bloggs", // Name        
+                "10 City Road", "Staines",
+                "Middlesex", "AB1 2CD" // Address    
+            );
+
+            var order = new Order(1, customer);
+
+            // Act, Assert: not interesting for this example
+        }
+    }
+}
