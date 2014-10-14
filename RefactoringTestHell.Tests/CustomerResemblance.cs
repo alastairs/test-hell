9137a1f13fa632fa6ce69530a66f1190a893b2d6
Alastair Smith <alastair.smith@red-gate.com>
2014-10-14 15:14:59 +0100

    Define Equality on Customer and a Resemblance.
    The production Equality for Customers should be by ID, but for some tests
    we might be interested in name instead.

---
+++ RefactoringTestHell.Tests/CustomerResemblance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RefactoringTestHell.Tests
+{
+    class CustomerResemblance : Customer, IEquatable<Customer>
+    {
+        public CustomerResemblance(int id, string givenName, string familyName, string addressLine1, string addressLine2,
+            string addressLine3, string postCode)
+            : base(id, givenName, familyName, addressLine1, addressLine2, addressLine3, postCode)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Customer);
+        }
+
+        new public bool Equals(Customer other)
+        {
+            return GivenName == other.GivenName &&
+                   FamilyName == other.FamilyName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
--- RefactoringTestHell/Customer.cs
+++ RefactoringTestHell/Customer.cs
@@ -1,6 +1,8 @@
-﻿namespace RefactoringTestHell
+﻿using System;
+
+namespace RefactoringTestHell
 {
-    public class Customer
+    public class Customer : IEquatable<Customer>
     {
         public Customer(int id, string givenName, string familyName, string addressLine1, string addressLine2,
                         string addressLine3, string postCode)
@@ -14,6 +16,21 @@
             PostCode = postCode;
         }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Customer);
+        }
+
+        public bool Equals(Customer other)
+        {
+            return Equals(this, other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
         public int Id { get; private set; }
         public string GivenName { get; private set; }
         public string FamilyName { get; private set; }
@@ -21,5 +38,20 @@
         public string AddressLine2 { get; private set; }
         public string AddressLine3 { get; private set; }
         public string PostCode { get; private set; }
+
+        private static bool Equals(Customer a, Customer b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (a == null || b == null)
+            {
+                return false;
+            }
+
+            return a.Id == b.Id;
+        }
     }
 }
